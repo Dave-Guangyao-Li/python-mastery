20abc78780e6e9df6cee20d6256903b769ced474
Guangyao (Dave) Li <dave_guangyao_li@outlook.com>
2023-08-16 10:27:40 -0700

    ex1-5 and ex1-6

--- Solutions/1_3/pcost.py
+++ Solutions/1_3/pcost.py
@@ -5,6 +5,7 @@ total_cost = 0.0
 with open('../../Data/portfolio.dat', 'r') as f:
     for line in f:
         fields = line.split()
+        # print(fields)
         nshares = int(fields[1])
         price = float(fields[2])
         total_cost = total_cost + nshares * price
